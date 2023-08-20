995ef386a43b391d72411109ee40d90237111be3
Caetano Henrique <caetanotus@gmail.com>
2023-08-20 16:26:28 -0300

    initial comit

---
+++ script.js
@@ -0,0 +1,22 @@
+
+     
+  function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+  
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+  
+    // substituir a imgagem
+    if (html.classList.contains("light")) {
+      // se tiver light mode, adicionar a imagem light
+      img.setAttribute("src", "./caetano-light.jpg")
+    } else {
+      // se tiver sem light mode, manter a imagem normal
+      img.setAttribute("src", "./caetano.jpg")
+    }
+}
+  
+    
+   
+    
